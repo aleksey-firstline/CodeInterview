5bfc735a74d1bc2d0a8474a2d855a397500cc61e
Aleksey Galatin <aleksey_galatin@epam.com>
2020-08-24 17:15:14 +0300

    LongestSubstringKDistinct

---
+++ CodeInterview/LongestSubstringKDistinct.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CodeInterview
+{
+    public class LongestSubstringKDistinct
+    {
+        [Theory]
+        [InlineData("aaabcd", 2, 4)]
+        [InlineData("aaabcd", 1, 3)]
+        [InlineData("fbcdrrrgg", 2, 5)]
+        [InlineData("abcd", 2, 2)]
+        public void FindLength(string str, int K, int expected)
+        {
+            var uniqChars = new Dictionary<char, int>();
+            var j = 0;
+            var maxLength = 0;
+
+            for (var i = 0; i < str.Length; i++)
+            {
+                var rightChar = str[i];
+                if (uniqChars.TryGetValue(rightChar, out var rightCharCount))
+                    uniqChars[rightChar] = rightCharCount + 1;
+                else
+                    uniqChars.Add(rightChar, 1);
+
+                while (uniqChars.Count > K)
+                {
+                    var leftChar = str[j];
+                    if (uniqChars.TryGetValue(leftChar, out var leftCharCount))
+                        uniqChars[leftChar] = leftCharCount - 1;
+
+                    if (uniqChars[leftChar] == 0)
+                        uniqChars.Remove(leftChar);
+
+                    j++;
+                }
+
+                maxLength = Math.Max(maxLength, i - j + 1);
+            }
+
+            Assert.Equal(maxLength, expected);
+        }
+    }
+}
---
+++ CodeInterview/SmallestSubarrayWithGivenSum.cs
@@ -0,0 +1,35 @@
+using Xunit;
+
+namespace CodeInterview
+{
+    public class SmallestSubarrayWithGivenSum
+    {
+        [Theory]
+        [InlineData(new []{ 2, 3, 8 }, 7, 1)]
+        [InlineData(new []{ 2, 3, 1 }, 7, 0)]
+        [InlineData(new []{ 2, 1, 5, 2, 3, 2 }, 7, 2)]
+        [InlineData(new []{ 2, 3, 6, 4 }, 1, 1)]
+        public void CalculateAverages(int[] arr, int S, int expected)
+        {
+            var minSize = 0;
+            var sum = 0;
+            var j = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+
+                while (sum >= S)
+                {
+                    var size = i + 1 - j;
+                    if (minSize > size || minSize == 0)
+                        minSize = size;
+
+                    sum -= arr[j];
+                    j++;
+                }
+            }
+
+            Assert.Equal(expected, minSize);
+        }
+    }
+}
