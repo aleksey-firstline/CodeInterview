f1096a38a321ef7c9a902cb98fd23e7ce5cd1f62
Aleksey Galatin <aleksey_galatin@epam.com>
2020-08-30 19:54:54 +0300

    Length Of Longest No Repeat Substring

---
+++ CodeInterview/LengthOfLongestNoRepeatSubstring.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CodeInterview
+{
+    public class LengthOfLongestNoRepeatSubstring
+    {
+        [Theory]
+        [InlineData("aaabcd", 4)]
+        [InlineData("aarrbb", 2)]
+        [InlineData("abcde", 5)]
+        public void FindLength(string str, int expected)
+        {
+            var chars = new Dictionary<char, int>();
+            var winStart = 0;
+            var maxSize = 0;
+
+            for (var winEnd = 0; winEnd < str.Length; winEnd++)
+            {
+                var rightChar = str[winEnd];
+                if (chars.ContainsKey(rightChar))
+                {
+                    winStart = Math.Max(winStart, chars[rightChar] + 1);
+                }
+
+                if (!chars.TryAdd(rightChar, winEnd))
+                {
+                    chars[rightChar] = winEnd;
+                }
+
+                maxSize = Math.Max(maxSize, winEnd - winStart + 1);
+            }
+
+            Assert.Equal(maxSize, expected);
+        }
+    }
+}
